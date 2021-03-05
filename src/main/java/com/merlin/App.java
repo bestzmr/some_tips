05bf926a39b8622bfd5f28a475499f64506e2a38
merlin <z2485782203@163.com>
2021-03-05 17:52:09 +0800

    lambda

--- src/main/java/com/merlin/App.java
+++ src/main/java/com/merlin/App.java
@@ -3,11 +3,34 @@ package com.merlin;
 /**
  * Hello world!
  *
+ * @author zhaoqiang
  */
 public class App 
 {
+    public int nn = 1;
+    public static class InnerClass {
+        public static int staticMem = 1;
+        public int num = 2;
+
+        public void printNum() {
+            System.out.println(num);
+            System.out.println(staticMem);
+        }
+    }
+
+//    public class InnerClass2 {
+//        public int num = 1;
+//        public static int staticNum = 2;
+//
+//        public static void printNum() {
+//
+//        }
+
+//    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        App.InnerClass innerClass = new InnerClass();
+        innerClass.printNum();
     }
 }
---
+++ src/main/java/com/merlin/Foo.java
@@ -0,0 +1,25 @@
+package com.merlin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Foo<T> {
+
+    void handle(final T t) {
+        System.out.println("handling " + t);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+
+        Foo<Integer> f = new Foo<>();
+
+        list.forEach(f::handle);             // compiles fine
+//        list.forEach(obj -> f.handle(obj));// compilation error
+
+        f = new Foo<>(); // reassign f
+
+    }
+}
